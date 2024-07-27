58fb7ae34f68e97bf13ee804860067262b0ad6ba
Vincent Halim <vincenthendrha@gmail.com>
2024-07-27 15:49:39 +0800

    feat: stack

--- lib/badminbot-stack.ts
+++ lib/badminbot-stack.ts
@@ -1,12 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 
 export class BadminbotStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const broadcastFunction = new lambda.Function(this, 'BroadcastReminder', {
+      runtime: lambda.Runtime.NODEJS_20_X, // Choose any supported Node.js runtime
+      code: lambda.Code.fromAsset('lambda'), // Points to the lambda directory
+      handler: 'broadcast.event', // Points to the 'hello' file in the lambda directory
+    });
+
+    const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
+      handler: helloWorldFunction,
+      proxy: false,
+    });
 
     // example resource
     // const queue = new sqs.Queue(this, 'BadminbotQueue', {
